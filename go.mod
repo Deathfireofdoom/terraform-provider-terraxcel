bf0a3db5d88dac6af73a41b1c2c72f0c85260929
deathfireofdoom <oskar.elvkull@gmail.com>
2023-10-06 19:44:05 +0200

    fixed provider

--- go.mod
+++ go.mod
@@ -2,13 +2,17 @@ module github.com/Deathfireofdoom/terraxcel
 
 go 1.21.1
 
-require github.com/hashicorp/terraform-plugin-framework v1.4.0
+require (
+	github.com/Deathfireofdoom/terraxcel-client v0.0.0-20231005184639-54417944dcbf
+	github.com/hashicorp/terraform-plugin-framework v1.4.0
+	github.com/hashicorp/terraform-plugin-log v0.9.0
+)
 
 require (
+	github.com/Deathfireofdoom/excel-client-go v0.0.0-20230926160419-e0ec9822c082 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/hashicorp/go-hclog v1.5.0 // indirect
 	github.com/hashicorp/terraform-plugin-go v0.19.0 // indirect
-	github.com/hashicorp/terraform-plugin-log v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
--- terraxcel/provider.go
+++ terraxcel/provider.go
@@ -2,8 +2,15 @@ package terraxcel
 
 import (
 	"context"
+	"go/types"
+	"os"
 
+	"github.com/Deathfireofdoom/terraxcel-client/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 // Ensure the implementation satisfies the expected interfaces
@@ -11,10 +18,16 @@ var (
 	_ provider.Provider = &terraxcelProvider{}
 )
 
+func NewProvider() provider.Provider {
+	return &terraxcelProvider{}
+}
+
 type terraxcelProvider struct {
 }
 
 type terraxcelProviderModel struct {
+	Host  types.String `tfsdk:"host"`
+	Token types.String `tfsdk:"token"`
 }
 
 func (p *terraxcelProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -22,4 +35,81 @@ func (p *terraxcelProvider) Metadata(_ context.Context, _ provider.MetadataReque
 }
 
 func (p *terraxcelProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"host": schema.StringAttribute{
+				Optional: false,
+			},
+			"token": schema.StringAttribute{
+				Optional:  false,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func (p *terraxcelProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	tflog.Info(ctx, "configuring TerraXcel client")
+
+	// get provider data from config
+	var config terraxcelProviderModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Debug(ctx, "creating TerraXcel client")
+
+	// check if user setup provider block or if default values should be used
+	host := os.Getenv("TERRAXCEL_HOST")
+	token := os.Getenv("TERRAXCEL_TOKEN")
+
+	if !config.Host.IsNull() {
+		host = config.Host.ValueString()
+	}
+
+	if !config.Token.IsNull() {
+		token = config.Token.ValueString()
+	}
+
+	// check if either provider block is configured or env var
+	if host == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("host"),
+			"Missing TerraXcel host",
+			"Missing host for TerraXcel-server, either configure provider-block or set TERRAXCEL_HOST environment variable",
+		)
+	}
+
+	if token == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("token"),
+			"Missing TerraXcel token",
+			"Missing token for TerraXcel-server, either configure provider-block or set TERRAXCEL_TOKEN environment variable",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// setting up terraxcel-client
+	clientConfig := client.ClientConfig{
+		BaseURL:   host,
+		AuthToken: token,
+	}
+	client, err := client.NewClient(&clientConfig)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create TerraXcel API client",
+			"An unexpected error occurred when creating the TerraXcel API client. "+
+				"TerraXcel Client Error: "+err.Error(),
+		)
+		return
+	}
+
+	// make client available for resources that needs it
+	resp.DataSourceData = client
+	resp.ResourceData = client
 }
