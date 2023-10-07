4057b4250a8cc88e90568b34c7a8e5463c397616
deathfireofdoom <oskar.elvkull@gmail.com>
2023-10-07 10:17:02 +0200

    added datasource

--- terraxcel/extensions_data_source.go
+++ terraxcel/extensions_data_source.go
@@ -1,7 +1,11 @@
 package terraxcel
 
 import (
+	"context"
+
+	"github.com/Deathfireofdoom/terraxcel-client/client"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -15,6 +19,7 @@ func NewExtensionsDataSource() datasource.DataSource {
 }
 
 type extensionsDataSource struct {
+	client *client.Client
 }
 
 type extensionsDataSourceModel struct {
@@ -23,4 +28,53 @@ type extensionsDataSourceModel struct {
 
 type extensionModel struct {
 	Extension types.String `tfsdk:"extension"`
+}
+
+func (d *extensionsDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_extensions"
+}
+
+func (d *extensionsDataSource) Schema(_ context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"extensions": schema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"extension": schema.StringAttribute{Computed: true},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *extensionsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state extensionsDataSourceModel
+
+	extensions := d.client.GetExtensions()
+
+	// maps response from client to state
+	for _, extension := range extensions {
+		extensionState := extensionModel{
+			Extension: types.StringValue(extension),
+		}
+		state.Extensions = append(state.Extensions, extensionState)
+	}
+
+	// set stat
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+}
+
+func (d *extensionsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	d.client = req.ProviderData.(*client.Client)
 }
